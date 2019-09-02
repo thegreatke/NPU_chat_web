5be6c0a85a4bb27acff74ad8d3302bbaf2d7521e
angel-blip <1161638196@qq.com>
2019-09-02 14:59:14 +0800

    1
    1

--- spring-boot-websocket-chat-demo/src/main/resources/static/1/js/sign-up.js
+++ spring-boot-websocket-chat-demo/src/main/resources/static/1/js/sign-up.js
@@ -2,18 +2,20 @@ function  signup() {
 
    var username=$('#username').val();
     var password=$('#password').val();
-    var email=$('#gender').val();
+    var gender=$('#gender').val();
 
    $.ajax({
      type:"POST",
-       url:"127.0.0.1/regist?password="+password +"&gender="+email+"&userName="+username,
+       url:"127.0.0.1/regist?password="+password +"&gender="+gender+"&userName="+username,
        data:{},
-       datatype:"string",
+       datatype:"text",
        success:function(){
         alert("success");
     },
        error:function(XMLHttpRequest, textStatus, errorThrown) {
          alert(XMLHttpRequest.responseText);
+         alert(XMLHttpRequest.status);
+         alert(textStatus);
 
        }
    })
